c00e80cd00f8961a003b3960f271516aca36528f
Daniele Ciotti <Bighitti>
2024-04-14 10:14:04 +0200

    Tourist model

---
+++ src/main/java/it/unicam/ids/dciotti/model/Tourist.java
@@ -0,0 +1,39 @@
+package it.unicam.ids.dciotti.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Tourist {
+    private UUID cookie = UUID.randomUUID();
+    private Date firstInteraction = new Date();
+    private Date lastInteraction = new Date(firstInteraction.getTime());
+    private Coordinates lastCoords;
+
+    public Tourist(Coordinates lastCoords) {
+        this.lastCoords = lastCoords;
+    }
+
+    public UUID getCookie() {
+        return cookie;
+    }
+
+    public Date getFirstInteraction() {
+        return firstInteraction;
+    }
+
+    public Date getLastInteraction() {
+        return lastInteraction;
+    }
+
+    public void setLastInteraction(Date lastInteraction) {
+        this.lastInteraction = lastInteraction;
+    }
+
+    public Coordinates getLastCoords() {
+        return lastCoords;
+    }
+
+    public void setLastCoords(Coordinates lastCoords) {
+        this.lastCoords = lastCoords;
+    }
+}
