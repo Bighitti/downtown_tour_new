ee30b17ce06fcdf65a67168b7b8568359a6af0f2
Daniele Ciotti <Bighitti>
2024-04-19 21:39:10 +0200

    Contributor

---
+++ src/main/java/it/unicam/ids/dciotti/model/Contributor.java
@@ -0,0 +1,14 @@
+package it.unicam.ids.dciotti.model;
+
+import java.util.Date;
+
+public class Contributor {
+    private String firstname;
+    private String lastname;
+    private Date birthday;
+    private String email;
+    private String phone;
+    private String address;
+    private String fiscalCode;
+    private boolean authorized = false;
+}
