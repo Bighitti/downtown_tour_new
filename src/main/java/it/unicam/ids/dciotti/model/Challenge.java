17ff802a9b3215389087e6c36b1a8248e878ceac
Daniele Ciotti <Bighitti>
2024-04-26 18:25:27 +0200

    challenge

---
+++ src/main/java/it/unicam/ids/dciotti/model/Challenge.java
@@ -0,0 +1,10 @@
+package it.unicam.ids.dciotti.model;
+
+import java.util.Date;
+
+public class Challenge {
+    private String name;
+    private int maxPlayers;
+    private Date start;
+    private Date end;
+}
--- src/main/java/it/unicam/ids/dciotti/model/Entertainer.java
+++ src/main/java/it/unicam/ids/dciotti/model/Entertainer.java
@@ -1,4 +1,16 @@
 package it.unicam.ids.dciotti.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Entertainer extends User {
+    List<Challenge> challenges = new ArrayList<>();
+
+    public List<Challenge> getChallenges() {
+        return challenges;
+    }
+
+    public void setChallenges(List<Challenge> challenges) {
+        this.challenges = challenges;
+    }
 }
