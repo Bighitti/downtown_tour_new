8dc67f2863522d67d989854439b8d3f0433d2e5a
Daniele Ciotti <Bighitti>
2024-04-26 18:11:12 +0200

    Curator is a special kind of Contributor

--- src/main/java/it/unicam/ids/dciotti/model/Curator.java
+++ src/main/java/it/unicam/ids/dciotti/model/Curator.java
@@ -2,50 +2,13 @@ package it.unicam.ids.dciotti.model;
 
 import java.util.Date;
 
-public class Curator {
-    private String firstname;
-    private String lastname;
-    private Date birthday;
-    private String email;
-    private String password;
-
-    public String getFirstname() {
-        return firstname;
+public class Curator extends Contributor {
+    public Curator() {
+        super.setAuthorizedBy(this);
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    @Override
+    public void setAuthorizedBy(Curator authorizedBy) {
+        throw new RuntimeException("Curators are authorized by them self");
     }
 }
