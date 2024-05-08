de5d954e9444f33b02de92c0374f766475995e16
Daniele Ciotti <Bighitti>
2024-05-08 21:40:32 +0200

    entertainer may create challenges

--- src/main/java/it/unicam/ids/dciotti/model/Entertainer.java
+++ src/main/java/it/unicam/ids/dciotti/model/Entertainer.java
@@ -1,16 +1,28 @@
 package it.unicam.ids.dciotti.model;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Entertainer extends User {
-    List<Challenge> challenges = new ArrayList<>();
+    private final List<Challenge> challenges = new ArrayList<>();
+
+    public Challenge createChallenge(
+            String name,
+            String description,
+            int maxPlayers,
+            Date start,
+            Date end) {
+        Challenge challenge = new Challenge(this);
+        challenge.setName(name);
+        challenge.setDescription(description);
+        challenge.setMaxPlayers(maxPlayers);
+        challenge.setStart(start);
+        challenge.setEnd(end);
+        return challenge;
+    }
 
     public List<Challenge> getChallenges() {
         return challenges;
-    }
-
-    public void setChallenges(List<Challenge> challenges) {
-        this.challenges = challenges;
     }
 }
