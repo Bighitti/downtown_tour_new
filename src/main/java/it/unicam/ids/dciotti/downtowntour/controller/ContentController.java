3943d7823a1b4e734266e29b70a1482241e2398d
Daniele Ciotti <Bighitti>
2024-05-28 22:00:24 +0200

    feat: content controller created started

--- src/main/java/it/unicam/ids/dciotti/downtowntour/controller/ContentController.java
+++ src/main/java/it/unicam/ids/dciotti/downtowntour/controller/ContentController.java
@@ -1,10 +1,14 @@
 package it.unicam.ids.dciotti.downtowntour.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(path = "/content")
 public interface ContentController {
     //TODO: create
+    @PostMapping
+    ResponseEntity<Void> createContent();
     //TODO: read/retrieve
     //TODO: update
     //TODO: delete
--- src/main/java/it/unicam/ids/dciotti/downtowntour/controller/impl/ContentControllerImpl.java
+++ src/main/java/it/unicam/ids/dciotti/downtowntour/controller/impl/ContentControllerImpl.java
@@ -1,8 +1,24 @@
 package it.unicam.ids.dciotti.downtowntour.controller.impl;
 
 import it.unicam.ids.dciotti.downtowntour.controller.ContentController;
+import it.unicam.ids.dciotti.downtowntour.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ContentControllerImpl implements ContentController {
+    private final ContentService contentService;
+
+    @Autowired
+    public ContentControllerImpl(ContentService contentService) {
+        this.contentService = contentService;
+    }
+
+    @Override
+    public ResponseEntity<Void> createContent() {
+        System.out.println("Create Content Hit");
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
