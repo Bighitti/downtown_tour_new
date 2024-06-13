29e8bbbdcc07ce0f80f3e26a7930d9ad0bc61e14
Daniele Ciotti <Bighitti>
2024-06-13 20:22:00 +0200

    feat: db script (creation is just an example)

---
+++ db/create-contributor.sql
@@ -0,0 +1,17 @@
+USE downtown_tour;
+CREATE TABLE IF NOT EXISTS contributor (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR(64) NOT NULL,
+    lastname VARCHAR(64) NOT NULL,
+    birthday DATE NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    phone VARCHAR(25),
+    address VARCHAR(255),
+    fiscal_code VARCHAR(16) NOT NULL,
+    curator BOOL NOT NULL DEFAULT FALSE,
+    authorized_by INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (authorized_by) REFERENCES contributor (id),
+    CONSTRAINT CONTRIBUTOR_UNIQUE_TAX UNIQUE (fiscal_code),
+    CONSTRAINT CONTRIBUTOR_UNIQUE_EMAIL UNIQUE (email)
+);
